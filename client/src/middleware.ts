409af19335c05300a85ba1f370a2e94c9824fc6c
AndyC <ychen276@students.cs.ubc.ca>
2024-09-14 02:45:40 -0700

    Set up auth for front and back end

---
+++ client/src/middleware.ts
@@ -0,0 +1,9 @@
+import {withAuth} from "@kinde-oss/kinde-auth-nextjs/middleware";
+export default function middleware(req) {
+  return withAuth(req, {
+    isReturnToCurrentPage: true
+  });
+}
+export const config = {
+  matcher: ["/dashboard", "/client"]
+};
---
+++ server/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { jwtVerify } from "@kinde-oss/kinde-node-express";
+import cors from "cors";
+
+const app = express();
+const port = 3001;
+
+const corsOptions = {
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+};
+
+const jwtVerifier = jwtVerify(`https://collablauncher.kinde.com`, {
+  audience: "",
+});
+
+app.use(cors(corsOptions));
+
+app.get("/api/protected", jwtVerifier, (req, res) => {
+  console.log("Valid Token!");
+  res.json({
+    message: "You have accessed the protected endpoint!",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
