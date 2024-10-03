9977cf0ff7556592ed8ed3ed2b251567c827e27a
AndyC <ychen276@students.cs.ubc.ca>
2024-10-02 21:03:37 -0700

    finished preloader

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,11 +2,36 @@
 import style from "./page.module.scss";
 import Header from "../components/Header/index";
 import Landing from "../components/Landing/index";
+import Preloader from "../components/Preloader/index";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    setTimeout(() => {
+      setIsLoading(false);
+      document.body.style.cursor = "default";
+      document.body.style.overflow = "auto";
+      window.scrollTo(0, 0);
+      requestAnimationFrame(raf);
+    }, 2000);
+  }, []);
 
   return (
     <div className={style.main}>
+      <AnimatePresence mode="wait">
+        {isLoading && <Preloader />}
+      </AnimatePresence>
       <Header />
       <Landing />
     </div>
